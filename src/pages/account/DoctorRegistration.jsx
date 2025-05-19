f06e7307a637b75df387601b176226206cf0c5e2
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-05-20 04:47:11 +0600

    doctor registration done

--- src/pages/account/DoctorRegistration.jsx
+++ src/pages/account/DoctorRegistration.jsx
@@ -1,10 +1,276 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import ErrorAlert from "../../components/alert/ErrorAlert";
+import useAuthContext from "../../hooks/useAuthContext";
+import authApiClient from "../../services/auth-api-client";
 
 const DoctorRegistration = () => {
+  const { errorMsg } = useAuthContext();
+  const [nextPage, setNextPage] = useState(false);
+
+  // const [successMsg, setSuccessMsg] = useState("");
+  // const [isloading, setloading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    delete data.confirmPassword;
+    console.log(data);
+    try {
+      const response = await authApiClient.post("/doctors/", {
+        user: data,
+        specialization: data.specialization,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="text-black">
-      <h1 className="text-2xl">Register as a Doctor</h1>
-    </div>
+    <section className="bg-gray-50 dark:bg-gray-900 p-44 ">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        {/* <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img
+            className="w-8 h-8 mr-2"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            alt="logo"
+          />
+          VaxCare
+        </a> */}
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            {errorMsg && <ErrorAlert error={errorMsg} />}
+            {/* {successMsg && <SuccessAlert msg={successMsg} />} */}
+            {/* {successMsg && <SuccessModal msg={successMsg} />} */}
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Create an account
+            </h1>
+
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div>
+                <label
+                  htmlFor="first_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  First Name
+                </label>
+                <input
+                  id="first_name"
+                  type="text"
+                  placeholder="First Name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("first_name", {
+                    required: "First Name Required",
+                  })}
+                />
+                {errors.first_name && (
+                  <span className="label-text-alt text-error">
+                    {errors.first_name.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="last_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Last Name
+                </label>
+                <input
+                  id="last_name"
+                  type="text"
+                  placeholder="Last Name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("last_name", {
+                    required: "Last Name Required",
+                  })}
+                />
+                {errors.last_name && (
+                  <span className="label-text-alt text-error">
+                    {errors.last_name.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="name@company.com"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("email", { required: "Email Required" })}
+                />
+                {errors.email && (
+                  <span className="label-text-alt text-error">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="phone_number"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Phone Number
+                </label>
+                <input
+                  id="phone_number"
+                  type="number"
+                  placeholder="+880 123 456 789"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("phone_number ")}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="address"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Address
+                </label>
+                <input
+                  id="address"
+                  type="text"
+                  placeholder="Dhaka, Bangladesh"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("address")}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("password", {
+                    required: "Password is Required",
+                    minLength: {
+                      value: 8,
+                      message: "Password must be at least 8 characters",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <span className="label-text-alt text-error">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="confirm-password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Confirm password
+                </label>
+                <input
+                  id="confirm-password"
+                  type="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("confirmPassword", {
+                    required: "Confirm Password is Required",
+                    validate: (value) =>
+                      value === watch("password") || "password do not match",
+                  })}
+                />
+                {errors.confirmPassword && (
+                  <span className="label-text-alt text-error">
+                    {errors.confirmPassword.message}
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="specialization"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Specialization
+                </label>
+                <input
+                  id="specialization"
+                  type="text"
+                  placeholder="Dhaka, Bangladesh"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  {...register("specialization", {
+                    required: "Skill Field Required",
+                  })}
+                />
+                {errors.specialization && (
+                  <span className="label-text-alt text-error">
+                    {errors.specialization.message}
+                  </span>
+                )}
+              </div>
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                    required=""
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label
+                    htmlFor="terms"
+                    className="font-light text-gray-500 dark:text-gray-300"
+                  >
+                    I accept the
+                    <a
+                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      href=""
+                    >
+                      Terms and Conditions
+                    </a>
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                // disabled={isloading}
+                onClick={handleSubmit(onSubmit)}
+              >
+                Create An Account
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <a
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Login here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
